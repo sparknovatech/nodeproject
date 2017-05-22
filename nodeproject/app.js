be553b9f48ea206c729d1eb0dc1e9c424a4b90a2
Muni Kumar N <nmunikumar@gmail.com>
2017-05-22 10:14:02 +0000

    express js configured

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,19 @@
-var aloha = "Aloha Variable";
-console.log(aloha);
+var express = require('express')
+var app = express();
+
+var port = process.env.PORT;
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+    
+});
+
+app.get('/routing',function(req,res){
+    res.send('Hello Routing');
+    
+});
+
+
+app.listen(port,function(err){
+   console.log('The Server is unning on port: ' + port); 
+});
