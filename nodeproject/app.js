c4374b27b4a0b14c5686d97db0ed240b059ce7a4
Muni Kumar N <nmunikumar@gmail.com>
2017-05-24 23:52:04 +0000

    bower installed

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -3,6 +3,10 @@ var app = express();
 
 var port = process.env.PORT;
 
+app.use(express.static('public'));  // setting static directory
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
 app.get('/',function(req,res){
     res.send('Hello World');
     
@@ -15,5 +19,5 @@ app.get('/routing',function(req,res){
 
 
 app.listen(port,function(err){
-   console.log('The Server is unning on port: ' + port); 
+   console.log('The Server is Running on port: ' + port); 
 });
